05d8bd2d70c19b8cd5cec24113e161feb6b42f8a
Oskar Ablimit <you@example.com>
2023-06-05 23:47:01 +0200

    CSRF

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['oskarcode.com','https://oskarcode.com/admin/login','https://*.oskarcode.com']
